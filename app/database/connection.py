002df169510aa6e6653efb0da8fda0ad0dc90a92
Jackson Vieira <jacksonvieiira12@gmail.com>
2023-10-22 16:23:15 -0300

    start tables

--- app/database/connection.py
+++ app/database/connection.py
@@ -1,7 +1,13 @@
 import psycopg
 from decouple import config
 
-from app.database.sqls import CREATE_TABLE_USERS
+from app.database.sqls import (
+    CREATE_AGENDA_TABLE,
+    CREATE_TABLE_AGENDA_DAY_HOUR,
+    CREATE_TABLE_APPOINTMENTS,
+    CREATE_TABLE_REQUESTS_APPOINTMENTS,
+    CREATE_TABLE_USERS,
+)
 
 POSTGRES_URI = config('POSTGRES_URI')
 
@@ -15,6 +21,12 @@ def get_db_connection():
 def start_db():
     with psycopg.connect(POSTGRES_URI) as conn:
         with conn.cursor() as cur:
-            for sql in [CREATE_TABLE_USERS]:
+            for sql in [
+                CREATE_TABLE_USERS,
+                CREATE_AGENDA_TABLE,
+                CREATE_TABLE_AGENDA_DAY_HOUR,
+                CREATE_TABLE_REQUESTS_APPOINTMENTS,
+                CREATE_TABLE_APPOINTMENTS
+            ]:
                 cur.execute(sql)
             conn.commit()
--- app/database/sqls.py
+++ app/database/sqls.py
@@ -1,5 +1,3 @@
-# Create initials table here
-
 CREATE_TABLE_USERS = """
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
@@ -10,72 +8,64 @@ CREATE TABLE IF NOT EXISTS users (
 )
 """
 
-CREATE_TABLE_PATIENT = """
-CREATE TABLE IF NOT EXISTS patient (
-    id SERIAL PRIMARY KEY
-)
-"""
+# CREATE_TABLE_PATIENT = """
+# CREATE TABLE IF NOT EXISTS patient (
+#     id SERIAL PRIMARY KEY
+# )
+# """
 
-CREATE_TABLE_PSYCHOLOGIST = """
-CREATE TABLE IF NOT EXISTS psychologist (
-    id SERIAL PRIMARY KEY,
-    email VARCHAR(100) NOT NULL,
-    full_name VARCHAR(100) NOT NULL,
-    city VARCHAR(50) NOT NULL,
-    is_staff BIT NOT NULL,
-    is_superuser BIT NOT NULL,
-    is_active BIT NOT NULL,
-    data_joined TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
-"""
+# CREATE_TABLE_PSYCHOLOGIST = """
+# CREATE TABLE IF NOT EXISTS psychologist (
+#     id SERIAL PRIMARY KEY,
+#     email VARCHAR(100) NOT NULL,
+#     full_name VARCHAR(100) NOT NULL,
+#     city VARCHAR(50) NOT NULL,
+#     is_staff BIT NOT NULL,
+#     is_superuser BIT NOT NULL,
+#     is_active BIT NOT NULL,
+#     data_joined TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+# )
+# """
 
-CREATE_TABLE_PATIENT_REQUEST_SESSION = """
-CREATE TABLE IF NOT EXISTS patient_request_session (
+CREATE_TABLE_REQUESTS_APPOINTMENTS = """
+CREATE TABLE IF NOT EXISTS requests_appointments (
     id SERIAL PRIMARY KEY,
-    psychologist_id VARCHAR(50) NOT NULL,
-    patient_id VARCHAR(50) NOT NULL,
-    start_date_time VARCHAR(50) NOT NULL,
-    end_date_time VARCHAR(50) NOT NULL,
+    agenda_day_hour_id INTEGER NOT NULL,
     status VARCHAR(50) NOT NULL,
-    description VARCHAR(50) NOT NULL,
     reason VARCHAR(50) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at DATETIME NOT NULL,
-    FOREIGN KEY (psychologist_id) REFERENCES psychologist (id),
-    FOREIGN KEY (patient_id) REFERENCES patient (id)
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (agenda_day_hour_id) REFERENCES agenda_day_hour (id)
 )
 """
 
-CREATE_TABLE_HISTORY_SESSION = """
-CREATE TABLE IF NOT EXISTS history_session (
+CREATE_TABLE_APPOINTMENTS = """
+CREATE TABLE IF NOT EXISTS appointments (
     id SERIAL PRIMARY KEY,
-    psychologist_id VARCHAR(50) NOT NULL,
-    patient_id VARCHAR(50) NOT NULL,
-    start_date_time VARCHAR(50) NOT NULL,
-    end_date_time VARCHAR(50) NOT NULL,
+    agenda_day_hour_id INTEGER NOT NULL,
     status VARCHAR(50) NOT NULL,
-    description VARCHAR(50) NOT NULL,
     reason VARCHAR(50) NOT NULL,
     anonymous BIT NOT NULL,
-    FOREIGN KEY (psychologist_id) REFERENCES psychologist (id),
-    FOREIGN KEY (patient_id) REFERENCES patient (id)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (agenda_day_hour_id) REFERENCES agenda_day_hour (id)
 )
 """
 
-CREATE_TABLE_AGENDA = """
+CREATE_AGENDA_TABLE = """
 CREATE TABLE IF NOT EXISTS agenda (
     id SERIAL PRIMARY KEY,
-    psychologist_id VARCHAR(50) NOT NULL,
-    FOREIGN KEY (psycholog_id) REFERENCES psychologist (id)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )
 """
+
 CREATE_TABLE_AGENDA_DAY_HOUR = """
 CREATE TABLE IF NOT EXISTS agenda_day_hour (
     id SERIAL PRIMARY KEY,
-    agenda_id VARCHAR(50) NOT NULL,
-    week_day VARCHAR(10) NOT NULL,
-    start_time DATETIME NOT NULL,
-    end_time DATETIME NOT NULL,
+    agenda_id INTEGER NOT NULL,
+    start_date_time TIMESTAMP NOT NULL,
+    end_date_time TIMESTAMP NOT NULL,
     FOREIGN KEY (agenda_id) REFERENCES agenda (id)
 )
 """
