fa418ea6751c4a08c4cf7c6146a5d836d4b05a74
Jackson Vieira <jacksonvieiira12@gmail.com>
2023-10-22 16:40:36 -0300

    change appoitment schemas

--- app/schemas/appoiment.py
+++ app/schemas/appoiment.py
@@ -1,25 +1,37 @@
 from pydantic import BaseModel
 
+"""
+# id SERIAL PRIMARY KEY,
+# agenda_day_hour_id INTEGER NOT NULL,
+# status VARCHAR(50) NOT NULL,
+# reason VARCHAR(50) NOT NULL,
+# anonymous BIT NOT NULL,
+# created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+# updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+# FOREIGN KEY (agenda_day_hour_id) REFERENCES agenda_day_hour (id)
+"""
 
 class Appoiment(BaseModel):
     status: bool
-    description: str
     reason: str
     anonymous: bool
 
 class AppoimentCreate(Appoiment):
     agenda_day_hour_id: int
-    psychologist_id: int
-    patient_id: int
+    # patient_id: int
 
 class AppoimentOutput(Appoiment):
     id: int
     agenda_day_hour_id: int
-    psychologist_id: int
-    patient_id: int
+    # start_date_time: str
+    # end_date_time: str
+    created_at: str
+    updated_at: str
+    # psychologist_id: int
+    # patient_id: int
 
 class AppoimentRequest(BaseModel):
     agenda_day_hour_id: int
-    psychologist_id: int
-    patient_id: int
     reason: str
+    # psychologist_id: int
+    # patient_id: int
