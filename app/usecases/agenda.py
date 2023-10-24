c5376fab593e06ed3be90d32363781e46c977709
Pedro Henrique <phsl12.rf@gmail.com>
2023-10-24 00:57:35 -0300

    usercases agenda day hour

--- app/usecases/agenda.py
+++ app/usecases/agenda.py
@@ -5,6 +5,8 @@ from psycopg import Connection
 from app.schemas.agenda import Agenda, AgendaOutput
 
 # make appoiment create sql and update sql
+INSERT_AGENDA_DAY_HOUR = "INSERT INTO agenda_day_hour (id, agenda_id, start_day_time, end_date_time) VALUES (?, ?, ?, ?)"
+SELECT_DAY_HOUR_FROM_AGENDA = "SELECT * FROM agenda_day_hour WHERE id = %(id)s"
 CREATE_AGENDA_SQL = "INSERT INTO agenda DEFAULT VALUES;"
 SELECT_ALL_AGENDAS_SQL = "SELECT * FROM agenda"
 DELETE_AGENDA_SQL = "DELETE FROM agenda WHERE id = %(id)s"
@@ -28,3 +30,19 @@ class AgendaUseCases:
         self.cursor.execute(SELECT_ALL_AGENDAS_SQL)
         agendas = self.cursor.fetchall()
         return agendas
+    
+    def insert_agenda_day_hour(self, id, agenda_id , start_day_time, end_date_time) -> None:
+        values = (id, agenda_id, start_day_time, end_date_time)
+        self.cursor.execute(INSERT_AGENDA_DAY_HOUR, values)
+        self.db_connection.commit()
+    
+    def get_agenda_day_hour_by_id(self, id : int) -> Agenda_Day_Hour_Output:
+        self.cursor.execute(SELECT_DAY_HOUR_FROM_AGENDA, {"id": id})
+        agenda = self.cursor.fetchone()
+        return Agenda_Day_Hour_Output(id=agenda[0])
+
+
+
+
+
+
